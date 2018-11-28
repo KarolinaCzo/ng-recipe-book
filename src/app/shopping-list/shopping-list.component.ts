57ba4d567064f75cf3f04bc9a3a8c1aa230c74fe
Karolina Czochańska <karolinaczochanska@gmail.com>
2018-11-28 23:28:10 +0100

    Make adding new ingredients work

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,13 +12,21 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, AfterViewInit {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
+  // @ViewChild('nameInput') nameInputValue: ElementRef;
+  // @ViewChild('amountInput') amountInputValue: ElementRef;
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {}
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
